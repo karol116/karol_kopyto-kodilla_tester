01da55d4f37057ccbd1bef958d54adf51ed3de27
karol116 <103003917+karol116@users.noreply.github.com>
2022-04-06 13:51:43 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Karol");
+        System.out.println("22");
+        System.out.println("K");
+    }
+}
+
