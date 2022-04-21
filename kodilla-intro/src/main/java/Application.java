cc9cf4c3da091aafe008aa13537cbaca408fd81e
Karol Kopyto <karol.kopyto@yahoo.com>
2022-04-21 17:14:33 +0200

    version7

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static void main(String[] args) {
+
+        ManApplication Adam = new ManApplication("Adam",40.5,178);
+        Adam.older30AndHigher160();
+    }
+  }
