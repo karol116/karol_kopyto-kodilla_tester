c5e8316550b3388fe5ec728667437484cd5cc0b8
Karol Kopyto <karol.kopyto@yahoo.com>
2022-04-25 01:44:05 +0200

    version10

--- kodilla-intro/src/main/java/UserDialogsColour.java
+++ kodilla-intro/src/main/java/UserDialogsColour.java
@@ -4,16 +4,22 @@ public class UserDialogsColour {
 
     public static String getColour() {
         Scanner scanner = new Scanner(System.in);
-        while(true) {
+        while (true) {
             System.out.println("Enter the first letter of colour");
             String firstLetter = scanner.next().trim().toUpperCase();
             switch (firstLetter) {
-                case "B": return "Brown";
-                case "G": return "Green";
-                case "O": return "Orange";
-                case "P": return "Purple";
-                case "W" : return "White";
-                case "Y" : return "Yellow";
+                case "B":
+                    return "Brown";
+                case "G":
+                    return "Green";
+                case "O":
+                    return "Orange";
+                case "P":
+                    return "Purple";
+                case "W":
+                    return "White";
+                case "Y":
+                    return "Yellow";
                 default:
                     System.out.println("Unidentified colour. Choose another one.");
             }
--- kodilla-intro/src/main/java/YourColour.java
+++ kodilla-intro/src/main/java/YourColour.java
@@ -1,7 +1,8 @@
 public class YourColour {
     public static void main(String[] args) {
-       UserDialogsColour yourColour = new UserDialogsColour();
-       String result = yourColour.getColour();
+
+        UserDialogsColour yourColour = new UserDialogsColour();
+        String result = yourColour.getColour();
         System.out.println(result);
     }
 }
