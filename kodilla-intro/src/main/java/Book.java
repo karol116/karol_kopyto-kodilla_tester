e3bf92881d393d5c21fee79820b61e4cb3c5cf97
Karol Kopyto <karol.kopyto@yahoo.com>
2022-04-26 00:38:21 +0200

    version14

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,12 +1,20 @@
 public class Book {
+
     private String author;
     private String title;
 
-    public static String of(String author, String title) {
-        return author + title;
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public static Book of(String author, String title) {
+        Book book = new Book(author, title);
+        return book;
     }
 
     public static void main(String[] args) {
-        Book.of("Małgorzata Rejmer", "Błoto słodsze niż miód");
+        Book book = Book.of("Małgorzata Rejmer", "\"Błoto słodsze niż miód\"");
+        System.out.println(book.title + " " + book.author);
     }
 }
--- kodilla-intro/src/main/java/GradesUp.java
+++ kodilla-intro/src/main/java/GradesUp.java
@@ -1,16 +1,16 @@
 public class GradesUp {
     public static void main(String[] args) {
         Grades grades = new Grades();
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
-        grades.add(10);
+        grades.add(5);
+        grades.add(2);
+        grades.add(1);
+        grades.add(4);
+        grades.add(4);
+        grades.add(4);
+        grades.add(4);
+        grades.add(1);
+        grades.add(5);
+        grades.add(1);
 
         System.out.println(grades.getAverage());
     }
