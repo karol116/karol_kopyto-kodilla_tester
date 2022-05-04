5633007bd31cfc2f0ecc718ecb5b19cc8c0632e3
Karol Kopyto <karol.kopyto@yahoo.com>
2022-05-05 01:35:16 +0200

    version30

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -13,24 +13,23 @@ public class CarsApplication {
         Random random = new Random();
         int carsArrayLength = random.nextInt(15) + 1;
         Car cars[] = new Car[carsArrayLength];
-        for (int i = 0; i < cars.length; i++) {
+
+        for (int i = 0; i < cars.length; i++)
             cars[i] = drawCar();
-        }
+
         for (Car car : cars)
             CarUtils.describeCar(car);
     }
 
     public static Car drawCar() {
-        {
-            Random random = new Random();
-            int drawnCarMake = random.nextInt(3);
-            int speed = random.nextInt(90) + 90;
-            if (drawnCarMake == 0)
-                return new Honda(speed);
-            else if (drawnCarMake == 1)
-                return new Subaru(speed);
-            else
-                return new Toyota(speed);
-        }
+        Random random = new Random();
+        int drawnCarMake = random.nextInt(3);
+        int speed = random.nextInt(90) + 90;
+        if (drawnCarMake == 0)
+            return new Honda(speed);
+        else if (drawnCarMake == 1)
+            return new Subaru(speed);
+        else
+            return new Toyota(speed);
     }
 }
