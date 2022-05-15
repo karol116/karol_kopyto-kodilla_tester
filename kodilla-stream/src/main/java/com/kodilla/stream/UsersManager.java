b7d063c030134c99ed4b8e37ecb61aba6455f963
Karol Kopyto <karol.kopyto@yahoo.com>
2022-05-16 00:02:17 +0200

    version46

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -1,10 +1,21 @@
 package com.kodilla.stream;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsersManager {
     public static void main(String[] args) {
-        UserRepository.getUserList()
+//        UserRepository.getUserList()
+        List<String> usernames = UserRepository.getUserList()
                 .stream()
-                .map(u -> u.getUsername())
-                .forEach(un -> System.out.println(un));
+                .filter(u -> u.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+//                .forEach(un -> System.out.println(un));
+                .collect(Collectors.toList());
+                System.out.println(usernames);
+    }
+
+    public static String getUserName(User user) {
+        return user.getUsername();
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.homework;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRepository {
+   public static List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Task A", LocalDate.of(2022, 4, 28), LocalDate.of(2022, 5, 7)));
+        tasks.add(new Task("Task B", LocalDate.of(2022, 4, 30), LocalDate.of(2022, 5, 4)));
+        tasks.add(new Task("Task C", LocalDate.of(2022, 5, 4), LocalDate.of(2022, 5, 12)));
+        tasks.add(new Task("Task D", LocalDate.of(2022, 5, 7), LocalDate.of(2022, 6, 3)));
+        tasks.add(new Task("Task E", LocalDate.of(2022, 5, 15), LocalDate.of(2022, 6, 2)));
+        tasks.add(new Task("Task F", LocalDate.of(2022, 6, 1), LocalDate.of(2022, 6, 17)));
+        tasks.add(new Task("Task G", LocalDate.of(2022, 6, 8), LocalDate.of(2022, 6, 22)));
+        return tasks;
+    }
+}
