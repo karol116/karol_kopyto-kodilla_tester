31264f865adcc3b07ea45b408ded2281079c6b0d
Karol Kopyto <karol.kopyto@yahoo.com>
2022-05-16 21:11:41 +0200

    version49

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -1,2 +1,23 @@
-package com.kodilla.optional.homework;public class Application {
+package com.kodilla.optional.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Student> student = new ArrayList<>();
+        student.add(new Student("John", new Teacher("Adam")));
+        student.add(new Student("Olivia", new Teacher("Adam")));
+        student.add(new Student("Joshua", null));
+        student.add(new Student("Daniel", new Teacher("Emily")));
+        student.add(new Student("Ethan", new Teacher("Adam")));
+        student.add(new Student("John", null));
+        student.add(new Student("Emma", new Teacher("Emily")));
+
+        for (Student studentTeacher : student) {
+            Optional<Teacher> teachers = Optional.ofNullable(studentTeacher.getTeacher());
+            System.out.println("Uczeń: " + studentTeacher.getName() + ", nauczyciel: " + teachers.orElse(new Teacher("<undefined>")));
+        }
+    }
 }
--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Teacher.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Teacher.java
@@ -1,2 +1,18 @@
-package com.kodilla.optional.homework;public class Teacher {
+package com.kodilla.optional.homework;
+
+public class Teacher {
+    String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
