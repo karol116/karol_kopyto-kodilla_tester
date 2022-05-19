dd16d57d30e081e851c7be3b2d45b8e592184dce
Karol Kopyto <karol.kopyto@yahoo.com>
2022-05-19 17:51:19 +0200

    version53

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,13 @@
+package com.kodilla.exception.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarehouseTestSuite {
+    @Test
+    public void doesOrderExist_withException() {
+        Warehouse warehouse = new Warehouse();
+        assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("0101"));
+    }
+}
