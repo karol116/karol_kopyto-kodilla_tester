b506a4f51f32bbe0572524c83dd92151061aca63
Karol Kopyto <karol.kopyto@yahoo.com>
2022-05-21 02:40:35 +0200

    version59

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -1,7 +1,6 @@
 package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
